88a27f938444976deef38af55a817c9a2e81dc26
Matheus Souza <matheus.souza89@fatec.sp.gov.br>
2022-05-28 13:40:53 -0300

    refactor:  ajustando configuração do sass bootstrap

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/scss/bootstrap.scss'
 import './assets/scss/Main.scss'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
