ba1c8e67b866dd90afcbdb4e11fff48c78b2ed95
Matheus Souza <matheus.souza89@fatec.sp.gov.br>
2022-05-27 23:28:33 -0300

    feat: inicio da criação da tela de login e estilos

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,7 +1,43 @@
 import React from 'react'
+import { Button, Col, Input, Row } from 'reactstrap'
+import { AiFillFacebook } from 'react-icons/ai'
+import { FcGoogle } from 'react-icons/fc'
+import logo from 'assets/img/login.png'
 
 const Login = () => {
-  return <div>Login</div>
+  return (
+    <div className="d-flex justify-content-center align-items-center min-vh-100">
+      <div className="content-login">
+        <Row>
+          <Col xl={6}>
+            <div className="illustration">
+              <img src={logo} />
+            </div>
+          </Col>
+          <Col xl={6}>
+            <div className="form-login">
+              <h1>Faça seu login</h1>
+              <Input type="text" placeholder="E-mail" />
+              <Input type="text" placeholder="Senha" />
+              <Button className="btn-login w-100">ENTRAR</Button>
+              <a href="#" className="forgot-link">
+                Esqueci minha senha
+              </a>
+              <Button className="btn-facebook">
+                <AiFillFacebook /> Continuar com Facebook
+              </Button>
+              <Button className="btn-google">
+                <FcGoogle /> Continuar com Google
+              </Button>
+              <a href="#" className="signin-link">
+                Ainda não tem cadastro? Inscreva-se aqui
+              </a>
+            </div>
+          </Col>
+        </Row>
+      </div>
+    </div>
+  )
 }
 
 export default Login
